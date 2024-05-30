3c6b2e200027b942b2db99fdfb9164f324c31624
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2024-05-30 11:58:32 +0700

    case untuk pilihan 3

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -123,4 +123,8 @@ int main()
                 q.remove();
                 break;
             }
+            case '3': {
+                q.display();
+                break;
+            }
             
