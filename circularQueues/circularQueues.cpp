371c36363db5c33037008063a0d4e3cccd97fd71
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2024-05-30 11:57:37 +0700

    case untuk pilihan 1

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -114,4 +114,9 @@ int main()
             cin >> ch;
             cout << endl;
 
-           
+            switch (ch) {
+            case '1': {
+                q.insert();
+                break;
+            }
+            
